79d5ce7a0d8c58fa660a335b7a4bba456975be20
Ostroverkhov Andrey <andre.2233@mail.ru>
2023-07-19 06:28:46 +0700

    feat(backend): добавлены новые сущности

---
+++ HW.Backend.DAL/Data/Entities/CorrectSequenceUserAnswer.cs
@@ -0,0 +1,17 @@
+﻿namespace HW.Backend.DAL.Data.Entities;
+
+/// <summary>
+/// Entity for CorrectSequenceUserAnswer
+/// </summary>
+public class CorrectSequenceUserAnswer : UserAnswer
+{
+    /// <summary>
+    /// CorrectSequenceAnswer identifier
+    /// </summary>
+    public required CorrectSequenceAnswer CorrectSequenceAnswer { get; set; }
+    /// <summary>
+    /// User's order in answer
+    /// </summary>
+    public required int Order { get; set; }
+
+}
