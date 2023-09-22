23cf58c20ce5b18152cdbaab4c03591f94ab3a88
Ostroverkhov Andrey <andre.2233@mail.ru>
2023-09-22 21:54:01 +0700

    fix(backendActivity): fix service

--- HW.Backend.BL/Services/ActivityService.cs
+++ HW.Backend.BL/Services/ActivityService.cs
@@ -5,6 +5,8 @@ using HW.Common.Exceptions;
 using HW.Common.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HW.Backend.BL.Services;
 
@@ -21,15 +23,30 @@ public class ActivityService : IActivityService
 
     public async Task<YearActivityDto> GetUserActivity(Guid userId)
     {
+        DateTime NowDate = DateTime.UtcNow;
+        DateTime YearAgoDate = DateTime.UtcNow.AddDays(-365);
+
         var user = await _dbContext.Students
             .FirstOrDefaultAsync(u => u.Id == userId) ?? throw new NotFoundException("User with this id not found");
 
-        var userLearned = await _dbContext.Learned
-            .Where(u => u.LearnDate.HasValue && u.LearnedBy == user)
+        var userLearneds = await _dbContext.Learned
+            .Where(d => d.LearnDate.HasValue && d.LearnedBy == user && d.LearnDate >= YearAgoDate && d.LearnDate <= NowDate)
+            .GroupBy(u => DateOnly.FromDateTime((DateTime)u.LearnDate))
+            .Select(g => new
+            {
+                LearnDate = g.Key,
+                LearnedCount = g.Count()
+            })
             .ToListAsync();
 
         var userAnswers = await _dbContext.UserAnswerTests
-            .Where(s => s.AnsweredAt.HasValue && s.Student == user)
+            .Where(u => u.AnsweredAt.HasValue && u.Student == user && u.AnsweredAt >= YearAgoDate && u.AnsweredAt <= NowDate)
+            .GroupBy(n => DateOnly.FromDateTime((DateTime)n.AnsweredAt))
+            .Select(y => new
+            {
+                AnswerDate = y.Key,
+                AnswersCount = y.Count()
+            })
             .ToListAsync();
 
         var yearActivity = new YearActivityDto
@@ -38,32 +55,31 @@ public class ActivityService : IActivityService
             MaxActivity = 0
         };
 
-        for (var i = 365; i >= 0;  i--)
+        for(int i = 365; i >= 0; i--)
         {
             var tempDay = DateTime.UtcNow.AddDays(-i);
-
-            var userLearnedDayCount = userLearned
-                .Where(d => DateOnly.FromDateTime((DateTime)d.LearnDate) == DateOnly.FromDateTime(tempDay))
-                .Count();
-
-            var userAnswersDayCount = userAnswers
-                .Where(d => DateOnly.FromDateTime((DateTime)d.AnsweredAt) == DateOnly.FromDateTime(tempDay))
-                .Count();
 
             var dayActivity = new DayActivityDto
             {
                 Date = tempDay,
-                UserAnswers = userAnswersDayCount,
-                UserLearned = userLearnedDayCount
+                UserAnswers = 0,
+                UserLearned = 0
             };
 
             yearActivity.DayActivities.Add(dayActivity);
+        };
 
-            if (userAnswersDayCount + userLearnedDayCount > yearActivity.MaxActivity)
-            {
-                yearActivity.MaxActivity = userAnswersDayCount + userLearnedDayCount;
-            }
-        }
+        foreach (var userLearned in userLearneds)
+        {
+            yearActivity.DayActivities.Find(g => DateOnly.FromDateTime(g.Date) == userLearned.LearnDate).UserLearned = userLearned.LearnedCount;
+        };
+
+        foreach (var userAnswer in userAnswers)
+        {
+            yearActivity.DayActivities.Find(g => DateOnly.FromDateTime(g.Date) == userAnswer.AnswerDate).UserAnswers = userAnswer.AnswersCount;
+        };
+
+        yearActivity.MaxActivity = yearActivity.DayActivities.Max(d => d.UserAnswers + d.UserLearned);
 
         return yearActivity;
     }
